febbbd9f0767790135a342383f38fcd02996d1fd
Zain Afzal <zain.afz@gmail.com>
2017-07-27 11:48:52 +1000

    Added Week 1 stuff

---
+++ Week 1/face.c
@@ -0,0 +1,17 @@
+//~ ~
+//0 0
+// o
+// -
+
+//~ ~
+//0 0
+// o
+//\_/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+    printf ("~~~~~~~\n");
+    return EXIT_SUCCESS;
+}
