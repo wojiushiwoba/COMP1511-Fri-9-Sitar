6cef4e9eda79dafa7e515a52c2b710b175ffce3e
Zain Afzal <zain.afz@gmail.com>
2017-10-13 09:58:57 +1100

    Added post tute stuff for week 11

--- Week 11/gameStruct.c
+++ Week 11/gameStruct.c
@@ -1,5 +1,7 @@
 typedef struct _game {
-
+	int turn;
+	int player;
+	int cardsPerHand;
 } game;
 
 Game newGame () {
@@ -7,18 +9,46 @@ Game newGame () {
 }
 
 int currentTurn (Game game) {
-
+	return game->turn;
 }
 
-void playMove(Game game, playerMove move) {
+void playMove(Game g, playerMove move) {
+	g->turn++;							 
 
-}
+	if (g->direction == CLOCKWISE) {
+
+		g->player = g->player % NUM_PLAYERS;
+	} else {
+
+		g->player = g->player - 1;
+
+		if (g->player < 0) {
+
+			g->player = NUM_PLAYERS -1;
+		}
+	}
+
+}	
 
 int currentPlayer(Game game) {
-
+	return game->player;
 }
 
 
-void test(Game game) {
-	
+void test() {
+	Game game = newGame();
+	assert(currentTurn(game) == 0);
+	playMove();
+	assert(currentTurn(game) == 1);
+
+
+
+
+
+
+
+
+
+
+
 }
--- Week 11/linkedList.c
+++ Week 11/linkedList.c
@@ -15,13 +15,18 @@ typedef struct _node {
 
 
 // creating a node
-Node createNode(int value) {
-	return NULL;
+Node createNode(int v) {
+	Node new = calloc(1,sizeof(node));
+	new->value = v;
+	new->next = NULL;
+	return new;
 }
 
 // creating a list
 List createList() {
-	return NULL;
+	List new = calloc(1, sizeof(list));
+	new->head = NULL;
+	return new;
 }
 
 // adding nodes to a list
@@ -29,17 +34,34 @@ List createList() {
 // what might be useful
 void addNode(List l, Node n) {
 
+	// list is empty
+	if (l->head == NULL) {
+		l->head = n;
+	} else {
+		// list has some elements in it
+		Node curr = l->head;
+		while (curr->next != NULL) {
+			curr = curr->next;
+		}
+		curr->next = n;
+	}
+
 }
 
 // removing nodes from a list
 // what other functions might be useful?
 void removeNode(List l, Node n) {
-
+	
 }
 
 // traversing the list
 void printList(List l) {
-
+	Node curr = l->head;
+	while (curr != NULL) {
+		printf("%d -> ",curr->value);
+		curr = curr->next;
+	}
+	printf("NULL\n");
 }
 
 // tests
